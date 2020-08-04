5d9e63a4bf999b78d5b7ad082e76b2df657a7e00
Enrique Pastor Cisneros <enrique.pastor@worldline.com>
2020-08-04 13:29:45 +0200

    Improve styles on login form

--- src/features/auth/components/Form.tsx
+++ src/features/auth/components/Form.tsx
@@ -11,39 +11,49 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      width: '100vw',
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     form: {
+        boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+        backgroundColor: 'white',
         padding: '2%',
-        border: '1px solid grey',
         borderRadius: '4px',
-        width: '15%'
+        width: '24%',
+        height: '24%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center'
     },
     input: {
         width: '98%',
         marginBottom: '15px'
     },
     submit: {
-        width:'100%',
-        marginTop: '10px'
+        width: '25%',
+        height: '12%',
+        backgroundColor: '#f7b500',
+        color: 'white',
+        border: '0',
+        borderRadius: '3px',
+        marginTop: '10px',
     },
     error: {
         margin: '0',
         color: 'red'
     }
-  }));
-  
+}));
+
 
 type FormValues = Pick<User, 'name' | 'password'> & {};
 
 const mapStateToProps = (state: RootState) => ({
     isLoading: state.auth.isLoadingLogin,
     error: state.auth.error,
-  });
+});
 
 const dispatchProps = {
     createLoginUser: (values: FormValues) =>
@@ -61,11 +71,9 @@ const InnerForm: React.FC<Props & FormikProps<FormValues>> = props => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-            
+
             <Form className={classes.form}>
-            <div>
-                {error && <p className={classes.error}>There are some issues in the login</p>}
-            </div>
+
                 <div >
                     <label htmlFor="name">Login</label>
                     <br />
@@ -94,8 +102,11 @@ const InnerForm: React.FC<Props & FormikProps<FormValues>> = props => {
                     <ErrorMessage name="password" />
                 </div>
                 <button className={classes.submit} type="submit" disabled={!dirty || isSubmitting}>
-                    {isLoading ? 'Loading' : 'Submit'  }
-        </button>
+                    {isLoading ? 'Loading' : 'Submit'}
+                </button>
+                <div>
+                    {error && <p className={classes.error}>There are some issues in the login</p>}
+                </div>
             </Form>
         </div>
     );
