88bc4897136c550669293ed00ac43ccf640408cc
Enrique Pastor Cisneros <enrique.pastor@worldline.com>
2020-08-05 18:55:39 +0200

    Added firebase constant as enviroment variables

--- src/services/firebase-conf.ts
+++ src/services/firebase-conf.ts
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 const config = {
-    apiKey: "AIzaSyAmcdpIWQi4-lkj2DtnyNhwNDXTTI0HB3M",
-    authDomain: "test-login-d053d.firebaseapp.com",
-    databaseURL: "https://test-login-d053d.firebaseio.com",
-    projectId: "test-login-d053d",
-    storageBucket: "test-login-d053d.appspot.com",
-    messagingSenderId: "862673629889",
-    appId: "1:862673629889:web:8d189cfb80126126f2da40"
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
 };
 const app = firebase.initializeApp(config);
 
