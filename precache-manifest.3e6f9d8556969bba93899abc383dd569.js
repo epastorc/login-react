01573c09e4ba3d75e9da8741d99292a1f5ef095b
Enrique Pastor Cisneros <enrique.pastor@worldline.com>
2020-08-05 13:43:57 +0200

    Updates

---
+++ precache-manifest.3e6f9d8556969bba93899abc383dd569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a11570e930ac5ce6846ebba1bfeb89a",
+    "url": "/login-react/index.html"
+  },
+  {
+    "revision": "3c7d02ea3ff458d87ce6",
+    "url": "/login-react/static/css/2.c489bcc4.chunk.css"
+  },
+  {
+    "revision": "3bd015f36ce3bccb070b",
+    "url": "/login-react/static/css/main.b889c8f2.chunk.css"
+  },
+  {
+    "revision": "3c7d02ea3ff458d87ce6",
+    "url": "/login-react/static/js/2.ae429d0a.chunk.js"
+  },
+  {
+    "revision": "3bd015f36ce3bccb070b",
+    "url": "/login-react/static/js/main.2fe1222d.chunk.js"
+  },
+  {
+    "revision": "1d2e34b4b130870956be",
+    "url": "/login-react/static/js/runtime~main.6f10b612.js"
+  }
+]);
